5a830bbdca335aa4aa089b7d3764912a41c5d37c
bertini36 <albertopouquiros@gmail.com>
2016-09-11 16:21:13 +0200

    Post tensorsparkandra 1/2

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     if 'DJANGO_SETTINGS_MODULE' not in os.environ:
         os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                              'config.settings.production')
+                              'config.settings.local')
 
     from django.core.management import execute_from_command_line
 
