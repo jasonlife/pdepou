9645b22be4256af99c3e39490c4e122b8f6a713d
bertini36 <albertopouquiros@gmail.com>
2017-02-01 17:22:19 +0100

    Error Django debug toolbar

--- config/settings/local.py
+++ config/settings/local.py
@@ -30,6 +30,10 @@ INSTALLED_APPS += ('django_extensions',)
 # ------------------------------------------------------------------------------
 INSTALLED_APPS += ('debug_toolbar',)
 
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
 # EMAIL CONFIGURATION
 # ------------------------------------------------------------------------------
 EMAIL_HOST = 'localhost'
